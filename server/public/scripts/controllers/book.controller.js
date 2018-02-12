f137b0463ed2bb0ee42e6df7bc6a5f1b803b04b2
Amy Richardson <amyrichardson96@gmail.com>
2018-02-12 08:35:11 -0600

    add book button working, creates book object to send to server

--- server/public/scripts/controllers/book.controller.js
+++ server/public/scripts/controllers/book.controller.js
@@ -4,12 +4,24 @@ myApp.controller('BookController', ['UserService', 'BookService', function(UserS
     self.userService = UserService;
     self.bookService = BookService;
     self.goodreadsBooks = BookService.goodreadsBooks; 
-
+    self.continents = ['Africa', 'Asia', 'Australia', 'Europe', 'North America', 'South America'];
 
     //sends book search info to BookService
     self.findBooks = function(bookSearch) {
         BookService.findBooks(bookSearch);
     } //end findBooks
 
+    self.addBook = function(book, continent) {
+        let bookToAdd = {
+            title: book.best_book.title._text,
+            author: book.best_book.author.name._text,
+            continent: continent,
+            cover_url: book.best_book.image_url._text,
+            average_rating: book.average_rating._text,
+            year_published: book.original_publication_year._text
+        }
+        console.log('in add book', bookToAdd);
+        
+    }
   }]);
   
